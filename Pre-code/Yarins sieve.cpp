4cd452d6121e18d4b728aa9f4c6879bbc4502490
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2019-09-25 16:16:55 +0600

    Create Yarins sieve.cpp

---
+++ Pre-code/Yarins sieve.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXSIEVE 100000000 // All prime numbers up to this 
+#define MAXSIEVEHALF (MAXSIEVE/2) 
+#define MAXSQRT 5000 // sqrt(MAXSIEVE)/2 
+char a[MAXSIEVE/16+2]; 
+#define isprime(n) (a[(n)>>4]&(1<<(((n)>>1)&7))) // Works when n is odd 
+int prime[5761455 + 1];
+//have to check for even numbers
+void sieve2()
+{
+	int i,j; 
+	memset(a,255,sizeof(a)); 
+	a[0]=0xFE; 
+	for(i=1;i<MAXSQRT;i++) 
+		if (a[i>>3]&(1<<(i&7))) 
+			for(j=i+i+i+1;j<MAXSIEVEHALF;j+=i+i+1) 
+				a[j>>3]&=~(1<<(j&7));
+	int pos = 1;
+	prime[0] = 2;
+	for(int i = 3; i<=MAXSIEVE; i+=2)
+		if(isprime(i)) prime[pos++] = i;
+}
+
+int main()
+{
+    
+	return 0;
+}
