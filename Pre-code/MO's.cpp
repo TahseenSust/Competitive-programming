dd123dbdeb5820b3575335d0b664a28339dab2a4
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2019-10-04 23:08:06 +0600

    Update MO's.cpp

--- Pre-code/MO's.cpp
+++ Pre-code/MO's.cpp
@@ -49,3 +49,50 @@ vector<int> mo_s_algorithm(vector<Query> queries) {
     }
     return answers;
 }
+
+// MOs with update
+
+struct query {
+  int l, r, t, id; // t is number of updates before, id is this query's id
+} q[maxn];
+
+struct update {
+  int x, pre, now;
+} u[maxn];
+
+void apply(int x, int t) {
+  if(l <= x && x <= r) {  // l, r is the l, r from MO's algo
+    remove(x);
+    a[x] = y;
+    add(x);
+  } else a[x] = y;
+}
+
+bool cmp(query &a, query &b) {
+  int l1 = a.l / k, l2 = b.l / k,
+    r1 = a.r / k, r2 = b.r / k;
+  // Left blocks differ, they aren't in same group,
+  // first comes who have smaller left block.
+  if(l1 != l2) return l1 < l2;  
+  // So here we have same Left Block, but if Right Blocks aren't equal,
+  // then who have right block smaller comes first.
+  if(r1 != r2) return r1 < r2;
+  // Now we have l1 == l2 and r1 == r2,
+  // So both a and b query is in same group,
+  // sort them based on time.
+  return a.t < b.t;
+}
+
+int l = 0, r = -1, t = 0; // Note that these values may change
+                          // Depending on your implementation of other part
+for(int i = 0; i < Q; i++) {
+  while(t < q[i].t) t++, apply(u[t].x, u[t].now); // Forward Update
+  while(t > q[i].t) apply(u[t].x, u[y].pre), t--; // Reverse Update
+
+  while(l > q[i].l) add(--l);
+  while(r < q[i].r) add(++r);
+  while(l < q[i].l) remove(l++);
+  while(r > q[i].r) remove(r--);
+
+  ans[q[i].id] = some_variable;
+}
