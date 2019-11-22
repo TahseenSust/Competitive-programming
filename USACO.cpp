ec580297051199439f74e5037cbcc6edd955209f
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2019-11-22 21:10:47 +0600

    Create USACO.cpp

---
+++ USACO.cpp
@@ -0,0 +1,25 @@
+/*
+ID: tahseen1
+TASK: milk6
+LANG: C++14
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF=(1<<28);
+int dx[]={1,-1,0,0};
+int dy[]={0,0,1,-1};
+const int sz=35;
+
+
+int main()
+{
+
+    freopen("milk6.in","r",stdin);
+    freopen("milk6.out","w",stdout);
+
+
+
+
+}
