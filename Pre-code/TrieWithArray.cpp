4b2a6ec2d85b3603d86f3ab4651d527c5593e600
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2020-01-21 01:25:42 +0600

    Update TrieWithArray.cpp

--- Pre-code/TrieWithArray.cpp
+++ Pre-code/TrieWithArray.cpp
@@ -1,53 +1,61 @@
-// Rumman vai
-
 #include<bits/stdc++.h>
 using namespace std;
 
 struct data
 {
-    int tree[10];
+    int tree[28];
     int complete;
-}trie[100001];
- 
-string text;
- 
-int tot_node=0;
- 
-void Insert_trie(int node,int ind)
+}trie[500001];
+
+char text[25];
+
+int tot_node=1;
+
+
+void Insert_trie()
 {
-    if(text[ind]=='\0')
-    {
-        trie[node].complete=1;
-        return;
+    int node=0;
+    for(int i=0;text[i];i++){
+        int id=(int) (text[i]-'a');
+        if(trie[node].tree[id]==0){
+            trie[node].tree[id]=++tot_node;
+        }
+        node=trie[node].tree[id];
     }
-    int id=(int) (text[ind]-'0');
-    if(trie[node].tree[id]==-1) trie[node].tree[id]=++tot_node;
-    int nnode=trie[node].tree[id];
-    Insert_trie(nnode,ind+1);
+    trie[node].complete++;
 }
 
-bool Search_Trie(int node,int ind)
+
+int Search_Trie()
 {
-	if(text[ind]=='\0')
-	{
-		return trie[node].complete;
+    int node=1;
+	for(int i=0;text[i];i++){
+        int id=(int) (text[i]-'a');
+        if(trie[node].tree[id]==0) return 0;
+        node=trie[node].tree[id];
 	}
-	int id=(int) (text[ind]-'0');
-    if(trie[node].tree[id]==-1) return false;
-    int nnode=trie[node].tree[id];
-    return Search_Trie(nnode,ind+1);
+	return trie[node].complete;
 }
 
 int main(int argc, char const *argv[])
 {
-	memset(trie,-1,sizeof trie);
-	text="0154";
-	Insert_trie(0,0);
-	text="0153";
-	Insert_trie(0,0);
-	text="0152";
-	Insert_trie(0,0);
-	text="0155";
-	cout<<Search_Trie(0,0)<<endl;
-	return 0;
+	int t;
+	scanf("%d",&t);
+
+    for(int i=0;i<t;i++){
+        scanf("%s",text);
+        Insert_trie();
+    }
+
+    scanf("%d",&t);
+
+
+    for(int i=0;i<t;i++){
+        scanf("%s",text);
+        printf("%d\n",Search_Trie());
+    }
+
+
+
+
 }
