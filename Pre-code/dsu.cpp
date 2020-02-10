3afc42ac0bd443dc8ebb5b9eb8163c022886fdff
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2020-02-11 03:43:07 +0600

    Create dsu.cpp

---
+++ Pre-code/dsu.cpp
@@ -0,0 +1,127 @@
+Compress jumps along a segment / Painting subarrays offline
+for (int i = 0; i <= L; i++) {
+    make_set(i);
+}
+
+for (int i = m-1; i >= 0; i--) {
+    int l = query[i].l;
+    int r = query[i].r;
+    int c = query[i].c;
+    for (int v = find_set(l); v <= r; v = find_set(v)) {
+        answer[v] = c;
+        parent[v] = v + 1;
+    }
+}
+Support distances up to representative
+void make_set(int v) {
+    parent[v] = make_pair(v, 0);
+    rank[v] = 0;
+}
+
+pair<int, int> find_set(int v) {
+    if (v != parent[v].first) {
+        int len = parent[v].second;
+        parent[v] = find_set(parent[v].first);
+        parent[v].second += len;
+    }
+    return parent[v];
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a).first;
+    b = find_set(b).first;
+    if (a != b) {
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = make_pair(a, 1);
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+}
+Support the parity of the path length / Checking bipartiteness online
+void make_set(int v) {
+    parent[v] = make_pair(v, 0);
+    rank[v] = 0;
+    bipartite[v] = true;
+}
+
+pair<int, int> find_set(int v) {
+    if (v != parent[v].first) {
+        int parity = parent[v].second;
+        parent[v] = find_set(parent[v].first);
+        parent[v].second ^= parity;
+    }
+    return parent[v];
+}
+
+void add_edge(int a, int b) {
+    pair<int, int> pa = find_set(a);
+    a = pa.first;
+    int x = pa.second;
+
+    pair<int, int> pb = find_set(b);
+    b = pb.first;
+    int y = pb.second;
+
+    if (a == b) {
+        if (x == y)
+            bipartite[a] = false;
+    } else {
+        if (rank[a] < rank[b])
+            swap (a, b);
+        parent[b] = make_pair(a, x^y^1);
+        bipartite[a] &= bipartite[b];
+        if (rank[a] == rank[b])
+            ++rank[a];
+    }
+}
+
+bool is_bipartite(int v) {
+    return bipartite[find_set(v).first];
+}
+Offline RMQ (range minimum query) in O(α(n)) on average / Arpa's trick
+struct Query {
+    int L, R, idx;
+};
+
+vector<int> answer;
+vector<vector<Query>> container;
+//container[i] contains all queries with R == i.
+stack<int> s;
+for (int i = 0; i < n; i++) {
+    while (!s.empty() && a[s.top()] > a[i]) {
+        parent[s.top()] = i;
+        s.pop();
+    }
+    s.push(i);
+    for (Query q : container[i]) {
+        answer[q.idx] = a[find_set(q.L)];
+    }
+}
+Storing the DSU explicitly in a set list / Applications of this idea when merging various data structures
+vector<int> lst[MAXN];
+int parent[MAXN];
+
+void make_set(int v) {
+    lst[v] = vector<int>(1, v);
+    parent[v] = v;
+}
+
+int find_set(int v) {
+    return parent[v];
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (lst[a].size() < lst[b].size())
+            swap(a, b);
+        while (!lst[b].empty()) {
+            int v = lst[b].back();
+            lst[b].pop_back();
+            parent[v] = a;
+            lst[a].push_back (v);
+        }
+    }
+}
