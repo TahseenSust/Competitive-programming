271b0897ac417568e53bf8c19bf383645c1b41d2
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2020-05-31 09:37:51 +0600

    Create bitmask.cpp

---
+++ bitmask.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+
+typedef long long ll;
+
+bool check(int mask,int pos)
+{
+    return mask&(1<<pos);
+}
+
+int SET(int mask,int pos)
+{
+    return mask|(1<<pos);
+}
+
+int FLIP(int mask,int pos)
+{
+    return mask^(1<<pos);
+}
+
+
+
+
+int main()
+{
+
+}
