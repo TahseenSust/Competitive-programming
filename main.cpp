63b95c19c72ca26c63f5cadd45f2f3b1f58d2ee7
Tahseen Rasheed Chowdhury <35876394+TahseenSust@users.noreply.github.com>
2020-06-20 15:11:42 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ using namespace std;
 #define endl '\n'
 
 typedef long long ll;
+typedef pair<int,int> pii;
+/*
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ost; // set
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_multiset; // multiset
+
+*/
 
 
 int main()
